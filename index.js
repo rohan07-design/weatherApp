33648c509a988f29812db9b2b6a8bdfcaedb212a
Rohan <rohanparande007@gmail.com>
2026-02-10 19:26:50 +0530

    Weather icons are added dynamically

--- index.js
+++ index.js
@@ -16,18 +16,37 @@ app.use(express.json());
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 
+//change the image based on weather
+function getWeatherIcon(weather) {
+  switch (weather) {
+    case "Clear":
+      return "https://cdn-icons-png.flaticon.com/512/869/869869.png";
+    case "Clouds":
+      return "https://cdn-icons-png.flaticon.com/512/414/414825.png";
+    case "Rain":
+      return "https://cdn-icons-png.flaticon.com/512/3313/3313888.png";
+    case "Snow":
+      return "https://cdn-icons-png.flaticon.com/512/642/642102.png";
+    default:
+      return "https://cdn-icons-png.flaticon.com/512/1779/1779940.png";
+  }
+}
+
+
 app.get("/", (req, res) => {
   let city = "Pune";
   let temp = "26";
   let weather = "Sunny";
   let wind = "12";
   let huminity = "65";
+
   res.render("index.ejs", {
     city,
     temp,
     weather,
     wind,
     huminity,
+    icon: "https://cdn-icons-png.flaticon.com/512/1779/1779940.png"
   });
 });
 
@@ -40,16 +59,23 @@ app.post("/", async (req, res) => {
 
   let temp = (getData.data.main.temp-273.15).toFixed(2);
   let weather = getData.data.weather[0].main;
+  let weatherIcon = getData.data.weather[0].icon;
   let wind = getData.data.wind.speed;
   let huminity = getData.data.main.humidity;
+
+  const iconUrl = `https://openweathermap.org/img/wn/01d@2x.png`;
+  console.log(iconUrl);
   
   res.render("index.ejs", {
     city,
     temp,
     weather,
     wind,
-    huminity
+    huminity,
+    iconUrl,
+    icon : getWeatherIcon(weather)
   });
+//   city.body = "";
 });
 
 app.listen(port, () => {
